82fa40c7c0fe1566c6c4808dde5fbd94e2c0176e
Sriram <sriramkk999@gmail.com>
2020-12-05 15:04:47 +0530

    Updates

---
+++ precache-manifest.c1a45eff87b3247e963a087b73da4314.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "635ca38eef8f6472a1eb",
+    "url": "/Covid-19-Tracker/static/css/main.2d3c347c.chunk.css"
+  },
+  {
+    "revision": "635ca38eef8f6472a1eb",
+    "url": "/Covid-19-Tracker/static/js/main.c4fb6120.chunk.js"
+  },
+  {
+    "revision": "00edef08b5924811140c",
+    "url": "/Covid-19-Tracker/static/js/runtime~main.e54b7f2d.js"
+  },
+  {
+    "revision": "db81ef17d52de5eda327",
+    "url": "/Covid-19-Tracker/static/css/2.01456a52.chunk.css"
+  },
+  {
+    "revision": "db81ef17d52de5eda327",
+    "url": "/Covid-19-Tracker/static/js/2.0feab7a0.chunk.js"
+  },
+  {
+    "revision": "b8d44276f4387f8d678832baf5287dbd",
+    "url": "/Covid-19-Tracker/index.html"
+  }
+];
